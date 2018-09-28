eeaedd3e09d98cb0be949cedc5570d349526b955
JaneEvans <jinjin.ge@utexas.edu>
2018-09-28 16:14:51 -0700

    Init gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 const gulp = require("gulp");
+const sass = require("gulp-sass");
 
 gulp.task("default", function() {
   // code for your default task goes here
   console.log('Hello, World!');
+});
+
+gulp.task("styles", function(){
+  gulp
+    .src("sass/**/*.scss")
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(gulp.dest("./css"));
 });
